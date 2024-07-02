a824d3c70ef7221c6399d87e76e25aa51d58f77c
fullgukbap <2hanjunbum6@gmail.com>
2024-07-02 15:03:11 +0900

    feat: sockets braocast

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,29 @@
-console.log("hi");  
+const messageList = document.querySelector("ul");  
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅")
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New Message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnect from Server ❌")
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    
+}
+
+messageForm.addEventListener("submit", handleSubmit);
