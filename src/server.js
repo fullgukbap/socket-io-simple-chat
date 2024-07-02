694a6ad2b0aa8af69d5a79445e61f271b24fdbd6
fullgukbap <2hanjunbum6@gmail.com>
2024-07-02 15:54:53 +0900

    feat: socket chatting server

--- src/server.js
+++ src/server.js
@@ -18,11 +18,24 @@ const wss = new WebSocket.Server({server});
 const sockets = [];
 
 wss.on("connection", (socket) => {
-    sockets.push(socket);
     console.log("Connected to Browser ✅");
+
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+
     socket.on("close", () => console.log("Disconnected from the Browser ❌"))
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString("utf-8")));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg)
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+            default:
+        }
+
     });
 });
 
